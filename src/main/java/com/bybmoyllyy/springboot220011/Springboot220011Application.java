a9d7fa030f8ee24fef6925037d21fc71c30dcf4c
jukegbu1 <jukegbu1@gmail.com>
2019-03-18 21:28:47 -0400

    Initial commit

---
+++ src/main/java/com/bybmoyllyy/springboot220011/Springboot220011Application.java
@@ -0,0 +1,13 @@
+package com.bybmoyllyy.springboot220011;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot220011Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot220011Application.class, args);
+    }
+
+}
